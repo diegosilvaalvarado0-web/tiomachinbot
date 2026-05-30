da5deb5c359411f9445bdf15b5bf73bd621f7412
Diego <diegosilvaalvarado0@gmail.com>
2026-05-30 04:15:07 -0500

    Actualizacion del bot

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-from flask import Flask
 from telegram import Update
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    ContextTypes
-)
-import threading
-import asyncio
-import os
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = ("8845810807:AAGaIFGBRC2RBS6-Rnb-Fkuws0mK2zGJdKM")
+TOKEN = "8845810807:AAGaIFGBRC2RBS6-Rnb-Fkuws0mK2zGJdKM"
 
-# Flask
-app_web = Flask(__name__)
-
-@app_web.route("/")
-def home():
-    return "Bot activo ✅"
-
-# Telegram
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     mensaje = (
-        "✨ 🔥*GRUPO VIP*🔥 ✨\n\n"
+       "✨ 🔥*GRUPO VIP*🔥 ✨\n\n"
         "Desbloquea +1,000 videos exclusivos 🔥\n\n"
         "🇵🇪 PERÚ: S/25\n"
         " 💜 Yape | 📲 Plin\n\n"
@@ -36,30 +20,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_video(
         video=video,
-        caption=mensaje
+        caption=mensaje,
+        parse_mode="Markdown"
     )
 
-async def run_bot():
-    app = ApplicationBuilder().token(TOKEN).build()
+app = ApplicationBuilder().token(TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("start", start))
 
-    print("Bot encendido ✅")
-
-    await app.initialize()
-    await app.start()
-    await app.updater.start_polling()
-
-# Ejecutar bot
-def start_bot():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(run_bot())
-    loop.run_forever()
-
-# Hilo del bot
-threading.Thread(target=start_bot).start()
-
-# Flask
-port = int(os.environ.get("PORT", 10000))
-app_web.run(host="0.0.0.0", port=port)
+print("Bot encendido...")
+app.run_polling()
